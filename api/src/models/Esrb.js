ff2dcb5c9ec3300abf07f4627548b843ef0ab3c4
JulianG1808 <93946563+JulianG1808@users.noreply.github.com>
2022-06-20 19:45:05 -0300

    modified DB-model | Esrb | Datatypes.ENUM

--- api/src/models/Esrb.js
+++ api/src/models/Esrb.js
@@ -3,7 +3,9 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('esrb', {
         name:{
-            type: DataTypes.STRING
+            type: DataTypes.ENUM(
+                {values: ["Everyone", "Everyone 10+", "Teen", "Mature", "Adults Only", "Rating Pending"]
+            })
         }
     }, {timestamps: false})
 }
