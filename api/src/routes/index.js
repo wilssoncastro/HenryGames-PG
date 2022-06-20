4cb43ace1c2a122a49d3c0d9c64fa116111e577a
JulianG1808 <93946563+JulianG1808@users.noreply.github.com>
2022-06-20 19:42:33 -0300

    add routes | getEsrb

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
-const { getVideogames } = require('../controllers/getVideogames')
+const router = Router()
 const userRouters = require('../controllers/userRouters')
+
 //Importar todos los routers
 const { getVideogames } = require('../controllers/getVideogames')
 const postVideogames = require('../controllers/postVideogames')
 const getTags = require ('../controllers/getTags')
-
+const getEsrb = require('../controllers/getEsrb')
 
 //Rutas
 
@@ -13,5 +14,6 @@ router.get('/videogames', getVideogames)
 router.use('/users', userRouters)
 router.use('/postVideogames', postVideogames) 
 router.use('/tags', getTags)
+router.use('/Esrb', getEsrb)
 
 module.exports = router
