520d68fb8fbaa156276bab7f70514e0c6b090dae
nicotanoira <nicotanoira@gmail.com>
2022-06-20 15:38:50 -0300

    Card component, algo de rutas para poder renderizar en Home la Card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/home';
+import Library from './components/Library/library';
+import Detail from './components/Detail/detail';
+import Shopping_Cart from './components/Shopping-Cart/shopping_cart';
+import Profile from './components/Profile/profile';
 
 function App() {
   return (
-    <div className="App">
-      <h1>GameSteam</h1>
-    </div>
+      <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/store' element={<Library/>}/>
+          <Route exact path='/store/:id' element={<Detail/>}/>
+          <Route exact path='/my_cart/:id' element={<Shopping_Cart/>}/>
+          <Route exact path='/profile/:id' element={<Profile/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Card/card.jsx
+++ client/src/components/Card/card.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({ image, name }) {
   return (
-    <div>Card</div>
+    <div className='card'>
+        <h1>img</h1>
+        <img src={image}/>
+        <div>
+            <h4>Juego{name}</h4>
+            <button>...</button>
+        </div>
+    </div>
   )
 }
