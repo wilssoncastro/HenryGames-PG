1bccf62c2aec758c76381fb04b7e9099ea3c4c91
nicotanoira <nicotanoira@gmail.com>
2022-06-20 11:09:40 -0300

    Actualizado dev BD

--- api/src/models/Player.js
+++ api/src/models/Player.js
@@ -2,7 +2,7 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     sequelize.define('player',{
-        id:{
+        id: {
             type: DataTypes.UUID,
             allowNull: false,
             primaryKey: true,
