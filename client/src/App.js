660a1fd7306e4e18c45127b98671fa1b2a8410e8
nicotanoira <nicotanoira@gmail.com>
2022-06-20 16:20:03 -0300

    Relleno de rutas y componente card

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,21 @@ import Library from './components/Library/library';
 import Detail from './components/Detail/detail';
 import Shopping_Cart from './components/Shopping-Cart/shopping_cart';
 import Profile from './components/Profile/profile';
+import Log_In from './components/Form/log_in';
+import Sign_Up from './components/Form/sign_up';
 
 function App() {
   return (
       <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/' element={<Log_In/>}/>
+          <Route exact path='/home' element={<Home/>}/>
           <Route exact path='/store' element={<Library/>}/>
           <Route exact path='/store/:id' element={<Detail/>}/>
-          <Route exact path='/my_cart/:id' element={<Shopping_Cart/>}/>
+          <Route exact path='/sign_up' element={<Sign_Up/>}/>
           <Route exact path='/profile/:id' element={<Profile/>}/>
+          <Route exact path='/my_cart/:id' element={<Shopping_Cart/>}/>
         </Routes>
       </div>
     </BrowserRouter>
