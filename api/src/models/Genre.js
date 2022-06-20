6bd108f698a455a68afc60b53b957244fdae5e36
unknown <nicolasgonzalez470@gmail.com>
2022-06-20 01:08:41 -0300

    Model Genre

---
+++ api/src/models/Genre.js
@@ -0,0 +1,17 @@
+
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('genre', {
+        id: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
+        nombre:{
+            type: DataTypes.STRING
+        }
+        
+    })
+}
