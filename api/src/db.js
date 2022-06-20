80a39e4a24655f60b4618ff23e44745894c36b95
JulianG1808 <93946563+JulianG1808@users.noreply.github.com>
2022-06-20 14:02:12 -0300

    arreglo de base de datos

--- api/src/db.js
+++ api/src/db.js
@@ -30,7 +30,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Player, Videogame, Genre, Esrb_Rating} = sequelize.models;
+const { Player, Videogame, Genre, Esrb_Rating } = sequelize.models;
 
 // Aca vendrian las relaciones
 //Player.hasMany(Player)  En duda, es para amigos.
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,14 @@
 const { Router } = require('express')
-const { getVideogames } = require('../controllers/getVideogames')
-//Importar todos los routers
-
 const router = Router()
+
+//Importar todos los routers
+const { getVideogames } = require('../controllers/getVideogames')
+const { postVideogames } = require('../controllers/postVideogames.js')
+
 
 //Rutas
 
 router.get('/videogames', getVideogames)
+router.use('/postVideogame', postVideogames)
 
 module.exports = router
