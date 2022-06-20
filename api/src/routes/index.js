ee3efd672fe4ac44430e0d97c83bc12b4186d2e9
unknown <nicolasgonzalez470@gmail.com>
2022-06-20 18:54:54 -0300

    Arreglos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,13 @@ const { Router } = require('express')
 const { getVideogames } = require('../controllers/getVideogames')
 const userRouters = require('../controllers/userRouters')
 //Importar todos los routers
-const { getVideogames } = require('../controllers/getVideogames')
+
 const postVideogames = require('../controllers/postVideogames')
 const getTags = require ('../controllers/getTags')
 
 
 //Rutas
+const router = Router()
 
 router.get('/videogames', getVideogames)
 router.use('/users', userRouters)
