33c4d6ad4d1e11d67abde1feb6ef0b5283a66bbc
Nico <nicotanoira@gmail.com>
2022-06-22 19:13:01 -0300

    Agregando friend list al navbar

--- client/src/components/NavBar/navbar.jsx
+++ client/src/components/NavBar/navbar.jsx
@@ -1,65 +1,143 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData";
+// import { SidebarData } from "./SidebarData";
+import { FriendList } from "./FriendList";
 import { IconContext } from "react-icons/lib";
+import * as FaIcons from "react-icons/fa";
+import * as CgIcons from "react-icons/cg";
+import * as BsIcons from "react-icons/bs";
 import * as VscIcons from "react-icons/vsc";
 import * as MdIcons from "react-icons/md"
 import * as BiIcons from "react-icons/bi"
 import './navbar.css';
+import './friendlist.css'
 
 export default function NavBar() {
-    const [sidebar, setSidebar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
+  const [friendBox, setFriendBox] = useState(false);
 
-    const showSidebar = () => setSidebar(!sidebar)
+  const showSidebar = () => setSidebar(!sidebar);
+  const showFriendBox = () => setFriendBox(!friendBox);
 
-    return (
-        <div>
-            <IconContext.Provider value={{color: '#fff'}}>
 
-                {/* Burger Menu */}
-                <div className="navbar"> 
+  const SidebarData = [
+    {
+      title: 'Profile',
+      path: '#',
+      icon: <CgIcons.CgProfile/>,
+      className: 'nav-text',
+      onClick: showSidebar
+    },
+    {
+      title: 'Theme',
+      path: '#',
+      icon: <VscIcons.VscColorMode/>,
+      className: 'nav-text',
+      onClick: showSidebar
+    },
+    {
+      title: 'Language',
+      path: '#',
+      icon: <BsIcons.BsTranslate/>,
+      className: 'nav-text',
+      onClick: showSidebar
+    },
+    {
+      title: 'Friends',
+      path: '#',
+      icon: <FaIcons.FaUserFriends/>,
+      className: 'friends-text',
+      onClick: showFriendBox
+    }
+  ]
+      
 
-                  {/* Chat clickable */}
-                  <Link to="#">
-                    <BiIcons.BiChat className="navbar-icons" />
-                  </Link> 
+  return (
+      <div>
+          <IconContext.Provider value={{color: '#fff'}}>
 
-                  {/* ShoppingCart clickable */}
-                  <Link to="/my_cart">
-                    <MdIcons.MdOutlineShoppingCart className="navbar-icons" />
-                  </Link> 
+              {/* Burger Menu */}
+              <div className="navbar"> 
 
-                  {/* SideMenu Opener (three lines) */}
-                  <Link to='#' >
-                      <VscIcons.VscThreeBars className="navbar-icons" onClick={showSidebar}/>
-                  </Link>
-                </div>
+                {/* Chat clickable */}
+                <Link to="#">
+                  <BiIcons.BiChat className="navbar-icons" />
+                </Link> 
 
-                {/* Menu vertical invisible de izquierda a derecha con su logica */}
-                <nav className={sidebar ? 'side-menu active' : 'side-menu'}>
-                    <ul className="side-menu-items" onClick={showSidebar}>
-                        <li className="sidebar-toggle">
-                            <Link to="#" className="navbar-icons">
-                                <VscIcons.VscThreeBars />
-                            </Link>
-                        </li>
-                        {SidebarData.map((item, index) => {
+                {/* ShoppingCart clickable */}
+                <Link to="/my_cart">
+                  <MdIcons.MdOutlineShoppingCart className="navbar-icons" />
+                </Link> 
+
+                {/* SideMenu Opener (three lines) */}
+                <Link to='#' >
+                    <VscIcons.VscThreeBars className="navbar-icons" onClick={showSidebar}/>
+                </Link>
+              </div>
+
+              {/* Menu vertical invisible de izquierda a derecha con su logica */}
+              <nav className={sidebar ? 'side-menu active' : 'side-menu'}>
+                  <ul className="side-menu-items">
+                      <li className="sidebar-toggle" onClick={showSidebar}>
+                          <Link to="#" className="navbar-icons">
+                              <VscIcons.VscThreeBars />
+                          </Link>
+                      </li>
+                      {SidebarData.map((item, index) => {
+                          return (
+                              <li key={index} className={item.className} >
+                                  <Link to={item.path} onClick={item.onClick}>
+                                      {item.icon}
+                                      <span>{item.title}</span>
+                                  </Link>
+                              </li>
+                              
+                          )
+                      })}
+                      <nav className={friendBox ? 'friendBox active' : 'friendBox'}>
+                        <h3 className="friendBoxTitle">Friend List</h3>
+                        {FriendList.map((user, index) => {
                             return (
-                                <li key={index} className={item.className}>
-                                    <Link to={item.path}>
-                                        {item.icon}
-                                        <span>{item.title}</span>
-                                    </Link>
-                                </li>
+                                <ul className="FriendBox">
+                                    <li key={index} className={user.className}>
+                                        <Link to={user.path}>
+                                            {user.image}
+                                            <span className="userName">{user.name}</span>
+                                            <span className="userStatus">{user.status}</span>
+                                        </Link>
+                                    </li>
+                                </ul>
                             )
                         })}
-                    </ul>
-                </nav>
+                      </nav>
 
-            </IconContext.Provider>            
-        </div>
-    )
+                  </ul>
+              </nav>
+
+              {/* Friend List
+              <nav className={friendBox ? 'friendBox active' : 'friendBox'}>
+                  <ul className="side-menu-items" onClick={showSidebar}>
+                      <li className="sidebar-toggle">
+                          <Link to="#" className="navbar-icons">
+                              <VscIcons.VscThreeBars />
+                          </Link>
+                      </li>
+                      {SidebarData.map((item, index) => {
+                          return (
+                              <li key={index} className={item.className}>
+                                  <Link to={item.path}>
+                                      {item.icon}
+                                      <span>{item.title}</span>
+                                  </Link>
+                              </li>
+                          )
+                      })}
+                  </ul>
+              </nav>      */}
+          </IconContext.Provider>            
+      </div>
+  )
 }
 
 
