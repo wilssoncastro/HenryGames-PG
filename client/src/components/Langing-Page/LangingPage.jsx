74f766579884e51a5d2bf35f6fbf457459f60953
Joaquin Palacio <joaquinpalacio2311@gmail.com>
2022-06-24 12:50:02 -0300

    Landing Page, y ruta en app.js

--- client/src/components/Langing-Page/LangingPage.jsx
+++ client/src/components/Langing-Page/LangingPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./landing.css";
 
 export default function LandingPage() {
+  // falta ternario para saber si ya estas registrado o no. y dependiendo de ahi va a mostrar el registrarse
+  // o el login.
   return (
-    <div className="landingPage">
-      <h1 className="landingH1">Welcome to HenryGames</h1>
-      <Link to="/home">
-        <button className="boton">Press to start</button>
-      </Link>
-    </div>
+      <div className="landing">
+        <h1 className="landingH1">Welcome to HenryGames</h1>
+        <Link to="/home">
+          <button className="botonLanging">Press to start</button>
+        </Link>
+      </div>
   );
 }
