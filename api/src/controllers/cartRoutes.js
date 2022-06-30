63892d44d97ad5b1f7c78c0a98cbd1da91801ba5
Santiago Campos <santiagocampossnels@gmail.com>
2022-06-30 12:53:25 -0300

    Viendo lo del cart

--- api/src/controllers/cartRoutes.js
+++ api/src/controllers/cartRoutes.js
@@ -25,10 +25,11 @@ router.post('/add/:id/:idGames', async(req, res) =>{
     const {id, idGames} = req.params;
     try {
         const user = await Player.findByPk(id)
-        const game = await Videogame.findByPk(idGame)
+        const game = await Videogame.findByPk(idGames)
+        const gamesL = JSON.parse(localStorage.getItem('cart'));
 
         if(user && game){
-            await user.addCart(game)
+            await user.addCart(gamesL)
         }else{
             return res.send('No se encontro el usuario... Intentalo otra vez')
         }
@@ -36,28 +37,28 @@ router.post('/add/:id/:idGames', async(req, res) =>{
         return res.send('Juego agregado a carrito agregado')
 
     } catch (error) {
-        
-    }
-})
-
-router.delete('/delete/:id/:idGame', async(req, res) => {
-    const { id, idGame } = req.params
-
-    try {
-        const user = await Player.findByPk(id)
-        const game = await Videogame.findByPk(idGame)
-
-
-        if(user && game){
-            await user.removeCart(game)
-        }else{
-            return res.send('No se encontro el usuario... Intentalo otra vez')
-        }
-
-        return res.send('Juego descartado del carrito')
-    } catch (error) {
         res.send(error)
     }
 })
+
+// router.delete('/delete/:id/:idGame', async(req, res) => {
+//     const { id, idGame } = req.params
+
+//     try {
+//         const user = await Player.findByPk(id)
+//         const game = await Videogame.findByPk(idGame)
+
+
+//         if(user && game){
+//             await user.removeCart(game)
+//         }else{
+//             return res.send('No se encontro el usuario... Intentalo otra vez')
+//         }
+
+//         return res.send('Juego descartado del carrito')
+//     } catch (error) {
+//         res.send(error)
+//     }
+// })
 
 module.exports = router
