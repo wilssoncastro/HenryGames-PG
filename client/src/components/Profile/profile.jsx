245525c7c6ea7234c97bf58d1f96b2852d005c70
wilson castro <wilssoncastro@gmail.com>
2022-06-30 09:47:25 -0500

    sin camios

--- client/src/components/Profile/profile.jsx
+++ client/src/components/Profile/profile.jsx
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import swal from "sweetalert";
-import { getWishList, deleteWishList } from '../../redux/actions/index';
-import NavBar from "../NavBar/navbar";
-=======
 import React from 'react'
 import NavBar from '../NavBar/navbar'
 import { Link } from 'react-router-dom'
 import './profile.css'
->>>>>>> dev
 
 export default function Profile() {
   
@@ -37,24 +28,6 @@ export default function Profile() {
   console.log(list);
 
   return (
-<<<<<<< HEAD
-    <div>
-      <div>
-        <NavBar />
-      </div>
-      <br/>
-      <br/>
-      <br/>
-      <h3>Wish List :</h3>
-       { list[0]?.wishs.length !== 0 ? list[0]?.wishs.map((e) => 
-       <div> 
-         <h4>{e.name}</h4> 
-         <img src={e.image} height='100px' width='200px' />
-         <h3>{e.price}</h3>
-         <button onClick={() => handleOnClickDelete(e.id)}>Delete from Wish List</button>
-       </div> 
-       ) : "Your wish list is empty" }
-=======
     <div className='Profile'>
       <NavBar/>
       <ul className='Create-Videogame-Bttn'>
@@ -62,7 +35,6 @@ export default function Profile() {
           <span>Create Videogame</span>
         </Link>
       </ul>
->>>>>>> dev
     </div>
   )
 }
