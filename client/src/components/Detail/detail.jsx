5c434f70fa3768c7c48c5380ae19efdc46f79d36
unknown <nicolasgonzalez470@gmail.com>
2022-07-01 01:44:58 -0300

    comments y pequeños fixs

--- client/src/components/Detail/detail.jsx
+++ client/src/components/Detail/detail.jsx
@@ -14,6 +14,7 @@ import NavBar from "../NavBar/navbar";
 import './detail.css'
 import Carousel from 'react-elastic-carousel'
 import swal from 'sweetalert'
+import Comment from "../Comment/Comment";
 
 export default function Detail() {
   const dispatch = useDispatch();
@@ -26,6 +27,8 @@ export default function Detail() {
 
   const videogame = useSelector((state) => state.details);
   const list = useSelector((state) => state.wishList);
+  const actual_cart = useSelector((state) => state.cart)
+  
   let idProfile = localStorage.getItem("id");
   
   
@@ -216,6 +219,10 @@ export default function Detail() {
             <Link to="/store">
               <button>Back to the store</button>
             </Link>
+          </div>
+
+          <div>
+            <Comment id_game={id}/>
           </div>
         </div>
       ) : null}
