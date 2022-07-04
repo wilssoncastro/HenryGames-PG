2328a890f5228ff2592ebe4d44de30ff0380b000
Nico <nicotanoira@gmail.com>
2022-07-04 14:00:20 -0300

    dev merged

--- client/src/components/Shopping-Cart/shopping_cart.jsx
+++ client/src/components/Shopping-Cart/shopping_cart.jsx
@@ -6,7 +6,7 @@ import {Link, useNavigate} from 'react-router-dom'
 import { 
   delFromCart, 
   getCartById,
-  delAllFromCart,
+  deleteAllFromCart,
   postMercadoPago } 
 from '../../redux/actions'
 import swal from 'sweetalert'
@@ -83,7 +83,7 @@ export default function ShoppingCart() {
     if(typeof id_user === 'object'){
       localStorage.setItem('cart', JSON.stringify([]))
     }else{
-      dispatch(delAllFromCart())
+      dispatch(deleteAllFromCart())
     }
     navigate('/my_cart')
   }
