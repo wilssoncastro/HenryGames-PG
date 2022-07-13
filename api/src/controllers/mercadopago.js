ca7a2dc182e4c0c795c1ef7dc7ed2a9e3a6c842c
Geryer86 <german.af86@gmail.com>
2022-07-13 17:29:56 -0300

    caca

--- api/src/controllers/mercadopago.js
+++ api/src/controllers/mercadopago.js
@@ -6,10 +6,10 @@ const randomstring = require("randomstring");
 //SDK mercadopago
 const mercadopago = require('mercadopago');
 
-const { ACCESS_TOKEN_MP, BACKenv_URL, BASE_URL } = process.env;
+const { ACCESS_TOKEN_MP, BACK_URL, BASE_URL } = process.env;
 
 const hk_URL = "https://henrygames.herokuapp.com"
-const BACK_URL = hk_URL?hk_URL:BACKenv_URL
+//const BACK_URL = hk_URL?hk_URL:BACKenv_URL
 
 //agrega credenciales
 mercadopago.configure({
