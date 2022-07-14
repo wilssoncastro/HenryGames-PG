5024ee4d930984b55b0614c9408275c06cf8e14e
Geryer86 <german.af86@gmail.com>
2022-07-14 12:58:32 -0300

    aahhhhh

--- api/src/app.js
+++ api/src/app.js
@@ -73,8 +73,8 @@ const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env['GOOGLE_CLIENT_ID'],
     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-    callbackURL: `/auth/google/callback`,
-    //callbackURL: `${BACK_URL}/auth/google/callback`,
+    //callbackURL: `/auth/google/callback`,
+    callbackURL: `${BACK_URL}/auth/google/callback`,
     passReqToCallback: true
   },
   async function(request, accessToken, refreshToken, profile, done) {
