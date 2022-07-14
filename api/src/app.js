202b264c300b5af6c7c030690bbd47b271b44014
Geryer86 <german.af86@gmail.com>
2022-07-14 00:54:14 -0300

    ndea

--- api/src/app.js
+++ api/src/app.js
@@ -77,8 +77,8 @@ passport.use(new GoogleStrategy({
     clientID: process.env['GOOGLE_CLIENT_ID'],
     clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
     //callbackURL: `${BACK_URL}/google/callback`,
-    callbackURL: `${BACK_URL}/auth/google/callback`,
-    //passReqToCallback: true
+    callbackURL: `${BACK_URL}/google/callback`,
+    passReqToCallback: true
   },
   async function(request, accessToken, refreshToken, profile, done) {
     console.log('entre en la funcion de GoogleStrategy')
